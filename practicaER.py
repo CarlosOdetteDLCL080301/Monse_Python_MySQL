d935bec56e60d7a836585af543cb061c9aa7f005
CarlosOdetteDLCL080301 <carlosodettedlcl@gmail.com>
2023-10-17 15:35:39 -0600

    Planteamos el ejercicio

---
+++ practicaER.py
@@ -0,0 +1,8 @@
+"""
+Utilice los datos adjuntos (contiene 2 archivos cvs, uno para la historia y otro más reciente, la suma
+de ambos es el total de datos) para generar el diagrama ER e impleméntalo en MySQL.
+Agregué como entregables:
+1.- El código python de creación de sus entidades.
+2.- Un pantallazo de su diagrama ER en MySQL Workbench
+3.- El código SQL de creación de llaves primaria y relaciones.
+"""
