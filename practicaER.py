07234ef1ed677e6f9a748d59956171036b75e2a9
CarlosOdetteDLCL080301 <carlosodettedlcl@gmail.com>
2023-10-20 00:41:34 -0600

    Logra crear las tablas con el formato adecuado

--- practicaER.py
+++ practicaER.py
@@ -15,39 +15,6 @@ import pandas as pd
 #La conexión se hace con el siguiente formato"mysql+pymysql://usuario:contraseña@host:puerto/nombre_de_la_base_de_datos"
 engine = create_engine("mysql+pymysql://root:odette@127.0.0.1/monse")
 Base = declarative_base()
-#Damos las caracteristicas de la entidad de nuestra tabla de nuesta DB
-class User(Base):
-    __tablename__= "nyc_arrests"                #Asignamos un nombre a nuestra tabla
-    ARREST_KEY          = Column(Integer(),primary_key=True)     #Definimos de tipo INT
-    ARREST_DATE         = Column(VARCHAR(20))
-    PD_CD               = Column(Integer())     #Definimos de tipo INT
-    PD_DESC             = Column(VARCHAR(60))   #La maxima cadena encontrada fue de 54, pero se deja 60 para posibles casos
-    KY_CD               = Column(Integer())     #Definimos de tipo INT
-    OFNS_DESC           = Column(VARCHAR(100))   #La maxima cadena encontrada fue de 36, pero se deja 40 para posibles casos
-    LAW_CODE            = Column(VARCHAR(15))   #La maxima cadena encontrada fue de 10, pero se deja 15 para posibles casos
-    LAW_CAT_CD          = Column(VARCHAR(10))    #La maxima cadena encontrada fue de 1
-    ARREST_BORO         = Column(VARCHAR(1))    #La maxima cadena encontrada fue de 1
-    ARREST_PRECINCT     = Column(Integer())     #Definimos de tipo INT
-    JURISDICTION_CODE   = Column(Integer())     #Definimos de tipo INT
-    AGE_GROUP           = Column(VARCHAR(10))   #La maxima cadena encontrada fue de 10, pero se deja 15 para posibles casos
-    PERP_SEX            = Column(VARCHAR(1))    #La maxima cadena encontrada fue de 1
-    PERP_RACE           = Column(VARCHAR(45))   #La maxima cadena encontrada fue de 30, pero se deja 45 para posibles casos
-    X_COORD_CD          = Column(Integer())     #Definimos de tipo INT
-    Y_COORD_CD          = Column(Integer())     #Definimos de tipo INT
-    Latitude            = Column(DOUBLE())      #Como se maneja punto decimal, usamos un double para "precisión"
-    Longitude           = Column(DOUBLE())      #Como se maneja punto decimal, usamos un double para "precisión"
-    Lon_Lat             = Column(VARCHAR(400))   #La maxima cadena encontrada fue de 45, pero se deja 50 para posibles casos
-
-    #En caso de llamar esta función, retorna el nombre de la clase
-    def __str__(self):
-        return self.username
-    
-Session = sessionmaker(engine)
-#Existe la sesión, pero aun no es utilizada 
-session = Session()
-
-#Si se ejecuta este código, se crean las tablas en la DB
-Base.metadata.create_all(engine)
 
 #Cargo los datos del archivo csv en un dataframe
 data = pd.read_csv('Ejemplos/Date.csv')
@@ -66,13 +33,14 @@ ent_PD_DESC['PD_DESC'] = ent_PD_DESC['PD_DESC'].map(str)
 #Definiendo los tipos de datos
 dtypes = [Integer, VARCHAR(100), Integer]
 #Creamos una tabla en la DB con los datos del dataframe
-# ent_PD_DESC.to_sql(
-#     name='tbl_PD_DESC',
-#     con=engine,
-#     if_exists='replace',
-#     index=5000,
-#     dtype=dict(zip(ent_PD_DESC.columns, dtypes))
-# )
+ent_PD_DESC.to_sql(
+    name='tbl_PD_DESC',
+    con=engine,
+    if_exists='replace',
+    index=5000,
+    dtype=dict(zip(ent_PD_DESC.columns, dtypes))
+)
+
 ##################################################################################
 ### LAW_CODE
 ##################################################################################
@@ -87,13 +55,13 @@ ent_LAW_Code['LAW_CODE'] = ent_LAW_Code['LAW_CODE'].map(str)
 #Definiendo los tipos de datos
 dtypes = [Integer, VARCHAR(100)]
 #Creamos una tabla en la DB con los datos del dataframe
-# ent_LAW_Code.to_sql(
-#     name='tbl_LAW_Code',
-#     con=engine,
-#     if_exists='replace',
-#     index=5000,
-#     dtype=dict(zip(ent_LAW_Code.columns, dtypes))
-# )
+ent_LAW_Code.to_sql(
+    name='tbl_LAW_Code',
+    con=engine,
+    if_exists='replace',
+    index=5000,
+    dtype=dict(zip(ent_LAW_Code.columns, dtypes))
+)
 
 ##################################################################################
 ### AGE_GROUP
@@ -109,13 +77,13 @@ ent_AGE_GROUP['AGE_GROUP'] = ent_AGE_GROUP['AGE_GROUP'].map(str)
 #Definiendo los tipos de datos
 dtypes = [Integer, VARCHAR(100)]
 #Creamos una tabla en la DB con los datos del dataframe
-# ent_AGE_GROUP.to_sql(
-#     name='tbl_AGE_GROUP',
-#     con=engine,
-#     if_exists='replace',
-#     index=5000,
-#     dtype=dict(zip(ent_AGE_GROUP.columns, dtypes))
-# )
+ent_AGE_GROUP.to_sql(
+    name='tbl_AGE_GROUP',
+    con=engine,
+    if_exists='replace',
+    index=5000,
+    dtype=dict(zip(ent_AGE_GROUP.columns, dtypes))
+)
 
 ##################################################################################
 ### PERP_RACE
@@ -131,13 +99,13 @@ ent_PERP_RACE['PERP_RACE'] = ent_PERP_RACE['PERP_RACE'].map(str)
 #Definiendo los tipos de datos
 dtypes = [Integer, VARCHAR(100)]
 #Creamos una tabla en la DB con los datos del dataframe
-# ent_PERP_RACE.to_sql(
-#     name='tbl_PERP_RACE',
-#     con=engine,
-#     if_exists='replace',
-#     index=5000,
-#     dtype=dict(zip(ent_PERP_RACE.columns, dtypes))
-# )
+ent_PERP_RACE.to_sql(
+    name='tbl_PERP_RACE',
+    con=engine,
+    if_exists='replace',
+    index=5000,
+    dtype=dict(zip(ent_PERP_RACE.columns, dtypes))
+)
 
 ##################################################################################
 ### OFNS_DESC
@@ -153,13 +121,13 @@ ent_OFNS_DESC['OFNS_DESC'] = ent_OFNS_DESC['OFNS_DESC'].map(str)
 #Definiendo los tipos de datos
 dtypes = [Integer, VARCHAR(100)]
 # #Creamos una tabla en la DB con los datos del dataframe
-# ent_OFNS_DESC.to_sql(
-#     name='tbl_OFNS_DESC',
-#     con=engine,
-#     if_exists='replace',
-#     index=5000,
-#     dtype=dict(zip(ent_OFNS_DESC.columns, dtypes))
-# )
+ent_OFNS_DESC.to_sql(
+    name='tbl_OFNS_DESC',
+    con=engine,
+    if_exists='replace',
+    index=5000,
+    dtype=dict(zip(ent_OFNS_DESC.columns, dtypes))
+)
 print(data.head(2))
 print(ent_LAW_Code.head())
 entidadPrincipal = data.copy()
